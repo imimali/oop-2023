50dc9ff8e13acc9645ea3a2b6ab301a3d76db936
imimali <imimali18@gmail.com>
2023-03-18 20:28:49 +0200

    Add struct and tyepdef demo

--- c-intro/main.c
+++ c-intro/main.c
@@ -3,7 +3,27 @@
 //
 #include <stdio.h>
 
+struct Person {
+    int age;
+    char name[20];
+};
+
+// typedef <existing type> <alias> where now existing type is `struct Person` and alias is `Person1`;
+typedef struct Person Person1;
+
+// analogously
+
+typedef struct {
+    int age;
+    char name[20];
+} Person2;
+
+// where existing type now is struct { ... } and alias is Person2
+
+
 int main() {
+    struct Person p;
+    Person1 p1; // but no Person p;
     printf("Hello world!\n");
     return 0;
 }
