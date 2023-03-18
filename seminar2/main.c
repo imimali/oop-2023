6158a173df0cea537e4e64d995bbdccc07774acc
imimali <imimali18@gmail.com>
2023-03-18 22:33:33 +0200

    Add element example

--- seminar2/main.c
+++ seminar2/main.c
@@ -1,5 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
+typedef struct Element {
+    char *name;
+    int value;
+} Element;
+
 int main() {
-    return 0;
+    int rows = 2, columns = 3;
+
+    char *names[2][3] = {{"Simon Petrikov", "Finn", "Jake"},
+                         {"A",              "B",    "C"}};
+
+    // allocate the structure containing pointers to Elements. `elements` is a pointer to a pointer of Elements,
+    // because it will contain(point to) rows of the matrix, which are actually pointers to Elements.
+    Element **elements = (Element **) malloc(sizeof(Element *) * rows);
+    for (int i = 0; i < rows; i++) {
+        // each row will contain Elements, we need a pointer to that area
+        elements[i] = (Element *) malloc(sizeof(Element) * columns);
+
+        for (int j = 0; j < columns; j++) {
+            // each name will be dynamically allocated.
+            elements[i][j].name = (char *) malloc(sizeof(char) * strlen(names[i][j]));
+            elements[i][j].value = i + j;
+            strcpy(elements[i][j].name, names[i][j]);
+        }
+    }
+    // TODO deallocate
 }
