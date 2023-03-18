8d49feeae4423763e212783a5f378350b6e995ea
imimali <imimali18@gmail.com>
2023-03-18 20:56:18 +0200

    Add pointers example

--- c-intro/main.c
+++ c-intro/main.c
@@ -2,31 +2,22 @@
 // Created by Mali Imre Gergely on 02.03.2023.
 //
 #include <stdio.h>
-
-int f(int a) {
-    // a different address from the original argument's address, the same value
-    printf("Address of a: %x, value of a: %d\n", &a, a);
-    a += 1;//no effect on original
-    return a;
-}
-
-void f_that_can_modify(int *a) {
-    // all arguments are passed by value, we can mutate values from outside the original scope of the function
-    // by passing an address - still by value.
-    printf("Address of a: %x, value of a: %x\n", &a, a);
-    *a += 1; // dereference
-}
+#include <stdlib.h>
 
 
 int main() {
-    int k = 9;
-    printf("Address of k: %x, value of k: %d\n", &k, k);
-    int from_f = f(k);
-    // yet another address, same value
-    printf("Address of from_f: %x, value of from_f: %d\n", &from_f, from_f);
-    printf("Address of k after calling f: %x, value of k: %d\n", &k, k);
+    int numbers[100];
+    // a static array has the same value as its address
+    printf("Address of numbers: %x and value of numbers: %x is the same\n", numbers, &numbers);
 
-    f_that_can_modify(&k);
-    printf("Address of k after calling f that can modify: %x, value of k: %d\n", &k, k);
+    int *numbers_on_heap = malloc(sizeof(int) * 100);
+    // a dynamically allocated array has a different value from the variable's address
+    printf("Address of numbers on heap: %x and value of numbers on heap: %x is not the same\n", numbers_on_heap,
+           &numbers_on_heap);
+
+    int *numbers_address = &numbers;
+    // the variable `numbers_address` has a different address from the value it points to, which is `numbers`
+    printf("Just like address of numbers_address: %x and value of numbers: %x are different\n", &numbers_address,
+           numbers);
     return 0;
 }
